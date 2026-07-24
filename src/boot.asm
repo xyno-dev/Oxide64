2e66a4b2c09f4a3ed19ceb4b5eba40c025997517
Xyno-dev <zayanarshad88@gmail.com>
2026-07-24 20:07:06 +0100

    style: cleaned up assembly formatting

--- src/boot.asm
+++ src/boot.asm
@@ -68,34 +68,16 @@ check_multiboot:
     jmp error
 
 check_cpuid:
-    
-    
-
-    
     pushfd
     pop eax
-
-    
     mov ecx, eax
-
-    
     xor eax, 1 << 21
-
-    
     push eax
     popfd
-
-    
     pushfd
     pop eax
-
-    
-    
     push ecx
     popfd
-
-    
-    
     cmp eax, ecx
     je .no_cpuid
     ret
@@ -104,13 +86,10 @@ check_cpuid:
     jmp error
 
 check_long_mode:
-    
     mov eax, 0x80000000    
     cpuid                  
     cmp eax, 0x80000001    
     jb .no_long_mode       
-
-    
     mov eax, 0x80000001    
     cpuid                  
     test edx, 1 << 29      
@@ -144,6 +123,7 @@ set_up_page_tables:
 
     ; map each p2 table: 512 entries * 2MB = 1GB each
     mov ecx, 0
+
 .map_p2_0:
     mov eax, 0x200000
     mul ecx
@@ -152,8 +132,8 @@ set_up_page_tables:
     inc ecx
     cmp ecx, 512
     jne .map_p2_0
-
     mov ecx, 0
+
 .map_p2_1:
     mov eax, 0x200000
     mul ecx
@@ -163,8 +143,8 @@ set_up_page_tables:
     inc ecx
     cmp ecx, 512
     jne .map_p2_1
-
     mov ecx, 0
+
 .map_p2_2:
     mov eax, 0x200000
     mul ecx
@@ -174,8 +154,8 @@ set_up_page_tables:
     inc ecx
     cmp ecx, 512
     jne .map_p2_2
-
     mov ecx, 0
+
 .map_p2_3:
     mov eax, 0x200000
     mul ecx
@@ -185,30 +165,21 @@ set_up_page_tables:
     inc ecx
     cmp ecx, 512
     jne .map_p2_3
-
     ret
 
 enable_paging:
-    
     mov eax, p4_table
     mov cr3, eax
-
-    
     mov eax, cr4
     or eax, 1 << 5
     mov cr4, eax
-
-    
     mov ecx, 0xC0000080
     rdmsr
     or eax, 1 << 8
     wrmsr
-
-    
     mov eax, cr0
     or eax, 1 << 31
     mov cr0, eax
-
     ret
 
 global _start
